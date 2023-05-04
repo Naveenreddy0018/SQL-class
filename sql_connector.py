133742e7e803e7b78cd26f0cc4ab90b87219b1b7
Naveen Kumar Reddy Chinthakunta <89237138+Naveenreddy0018@users.noreply.github.com>
2023-05-05 00:29:56 +0530

    Update sql_connector.py

--- sql_connector.py
+++ sql_connector.py
@@ -12,3 +12,24 @@ show databases;
 use databasename;
 show tables
 """
+
+
+import mysql.connector
+
+db = mysql.connector.connect(host='localhost', user='root', password="Naveen@SQL12", database="sample")
+
+cusr = db.cursor()          
+cusr.execute("CREATE DATABASE Sample")    #Creation of database
+cusr.execute("CREATE TABLE sample1(id INT PRIMARY KEY, name VARCHAR(10), contact_no INT)")    #Creating table in database
+print("Table successfully created")
+a = "INSERT INTO sample1 VALUES(%s, %s, %s)"    #Inserting single row of data
+b = (101, "Naveen", 8234567)
+cusr.execute(a, b)
+print("Values are successfully inserted")
+a1 = "INSERT INTO sample1 VALUES(%s, %s, %s)"   #Inserting multiple values of data in table
+b1 = [(102, "Karthik", 8214569), (103, "Suhail", 856794), (104, "Sahil", 9843234)]
+cusr.executemany(a1, b1)
+print("Many values are inserted into table") 
+db.commit()             
+cusr.close()
+db.close()
